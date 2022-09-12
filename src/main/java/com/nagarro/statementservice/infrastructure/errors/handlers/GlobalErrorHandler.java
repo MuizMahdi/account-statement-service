a55540f9a821e944839e09373cc3c092848f1db2
Muizz <moezmahdy@gmail.com>
2022-09-12 13:00:50 +0400

    feat: add global exception and request logging

--- src/main/java/com/nagarro/statementservice/infrastructure/errors/handlers/GlobalErrorHandler.java
+++ src/main/java/com/nagarro/statementservice/infrastructure/errors/handlers/GlobalErrorHandler.java
@@ -1,6 +1,8 @@
 package com.nagarro.statementservice.infrastructure.errors.handlers;
 
 import com.nagarro.statementservice.infrastructure.errors.pojos.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,19 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
-import java.util.Locale;
 
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalErrorHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalErrorHandler.class);
 
     private final List<ErrorHandler> handlers;
 
     public GlobalErrorHandler(List<ErrorHandler> handlers) {
         this.handlers = handlers;
+        LOGGER.debug("Error handling is active with handlers: {}", this.handlers);
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleException(Throwable exception, WebRequest webRequest, Locale locale) {
+    public ResponseEntity<?> handleException(Throwable exception, WebRequest webRequest) {
         ErrorResponse errorResponse = null;
 
         // Handle known exceptions
@@ -35,6 +38,8 @@ public class GlobalErrorHandler {
         // Unhandled/unknown exceptions
         if (errorResponse == null) {
             errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unknown error has occurred");
+            LOGGER.debug("Exception Web Request: {}", webRequest);
+            LOGGER.error(exception.getMessage(), exception);
         }
 
         return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
